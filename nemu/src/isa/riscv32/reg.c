804dd27ee377180552db4ef8c2087758fd6987db
tracer-ysyx <tracer@ysyx.org>
2026-05-15 22:37:49 +0800

    > compile NEMU 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  22:37:49 up  3:33,  0 users,  load average: 0.10, 0.04, 0.01

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -25,6 +25,9 @@ const char *regs[] = {
 
 void isa_reg_display() {
   for (int i = 0; i < 32; i++) {
+    if (strcmp(regs[i], "$0") == 0) {
+      printf("%s:\t0x%08x\n", regs[i], cpu.gpr[i]);
+    }
     printf("$%s:\t0x%08x\n", regs[i], cpu.gpr[i]);
   }
 
