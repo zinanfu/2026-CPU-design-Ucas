d431d77660e1d16da62fe4eb898e7f5b06f1b542
tracer-ysyx <tracer@ysyx.org>
2026-05-16 21:30:33 +0800

    > compile NEMU 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:30:33 up  4:46,  0 users,  load average: 0.08, 0.04, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -187,7 +187,7 @@ word_t expr(char *e, bool *success) {
       tokens[i].type = TK_DEREF;
     }
     if (tokens[i].type == '-' && (i == 0 || tokens[i - 1].type == '+' || tokens[i - 1].type == '-' || tokens[i - 1].type == '*' || tokens[i - 1].type == '/' || tokens[i - 1].type == '(' || tokens[i - 1].type == TK_EQ || tokens[i - 1].type == TK_NEQ || tokens[i - 1].type == TK_AND)) {
-      // printf("There is a minor\n");
+      printf("There is a minor\n");
       tokens[i].type = TK_NEGATIVE;
     }
   }
