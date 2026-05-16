b9bbc0d257fb4fea9631656fe44890b2ff033c03
tracer-ysyx <tracer@ysyx.org>
2026-05-16 21:08:24 +0800

    > compile NEMU 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:08:24 up  4:24,  0 users,  load average: 0.00, 0.01, 0.00

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -21,62 +21,62 @@ void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
 
-int main(int argc, char *argv[]) {
-  /* Initialize the monitor. */
-#ifdef CONFIG_TARGET_AM
-  am_init_monitor();
-#else
-  init_monitor(argc, argv);
-#endif
-
-  /* Start engine. */
-  engine_start();
-
-  return is_exit_status_bad();
-}
-
-// word_t expr(char *e, bool *success);
-
 // int main(int argc, char *argv[]) {
-
-//   if (argc != 2) {
-//     printf("Usage: %s input_file\n", argv[0]);
-//     return 1;
-//   }
-
+//   /* Initialize the monitor. */
+// #ifdef CONFIG_TARGET_AM
+//   am_init_monitor();
+// #else
 //   init_monitor(argc, argv);
-//   FILE *fp = fopen(argv[1], "r");
-//   if (fp == NULL) {
-//     perror("fopen");
-//     return 1;
-//   }
+// #endif
 
-//   char expr_buf[65536];
-//   unsigned expected;
+//   /* Start engine. */
+//   engine_start();
 
-//   while (fscanf(fp, "%u %[^\n]", &expected, expr_buf) == 2) {
-//     // printf("test begin\n");
-//     printf("expr: %s\n", expr_buf);
-//     bool success = true;
-//     word_t result = expr(expr_buf, &success);
-
-//     // printf("expr is done\n");
-//     if (!success) {
-//       printf("Bad expression: %s\n", expr_buf);
-//       continue;
-//     }
-
-//     if (result != expected) {
-//       printf("Mismatch!\n");
-//       printf("expr: %s\n", expr_buf);
-//       printf("expected: %u\n", expected);
-//       printf("got: %u\n", result);
-//       return 1;
-//     }
-//   }
-
-//   printf("All tests passed!\n");
-
-//   fclose(fp);
-//   return 0;
+//   return is_exit_status_bad();
 // }
+
+word_t expr(char *e, bool *success);
+
+int main(int argc, char *argv[]) {
+
+  if (argc != 2) {
+    printf("Usage: %s input_file\n", argv[0]);
+    return 1;
+  }
+
+  init_monitor(argc, argv);
+  FILE *fp = fopen(argv[1], "r");
+  if (fp == NULL) {
+    perror("fopen");
+    return 1;
+  }
+
+  char expr_buf[65536];
+  unsigned expected;
+
+  while (fscanf(fp, "%u %[^\n]", &expected, expr_buf) == 2) {
+    // printf("test begin\n");
+    printf("expr: %s\n", expr_buf);
+    bool success = true;
+    word_t result = expr(expr_buf, &success);
+
+    // printf("expr is done\n");
+    if (!success) {
+      printf("Bad expression: %s\n", expr_buf);
+      continue;
+    }
+
+    if (result != expected) {
+      printf("Mismatch!\n");
+      printf("expr: %s\n", expr_buf);
+      printf("expected: %u\n", expected);
+      printf("got: %u\n", result);
+      return 1;
+    }
+  }
+
+  printf("All tests passed!\n");
+
+  fclose(fp);
+  return 0;
+}
