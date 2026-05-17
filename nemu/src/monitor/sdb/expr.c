86d8bf6dcaaa388e3bae08134205dd90b699ce5c
tracer-ysyx <tracer@ysyx.org>
2026-05-17 19:44:39 +0800

    > compile NEMU 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  19:44:39 up 52 min,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -366,7 +366,16 @@ int eval(int p, int q, bool *success) {
       case '+': return val1 + val2;
       case '-': return val1 - val2;
       case '*': return val1 * val2;
-      case '/': return val1 / val2;
+      case '/': {
+        if (val2 == 0) {
+          printf("Error: div 0\n");
+          return 0;
+        }
+        return val1 / val2;
+      }
+      
+      
+      
       case TK_EQ: return val1 == val2;
       case TK_NEQ: return val1 != val2;
       case TK_AND: return val1 && val2;
