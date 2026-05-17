d03f0cd0ab5ca752ef6eb1b23ac5530361eb146d
tracer-ysyx <tracer@ysyx.org>
2026-05-17 12:20:15 +0800

    > compile NEMU 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  12:20:15 up  2:33,  0 users,  load average: 0.01, 0.08, 0.08

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -30,6 +30,7 @@ int atoi(const char* nptr) {
 }
 
 
+uintptr_t addr = 0;
 
 
 void *malloc(size_t size) {
@@ -37,7 +38,18 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  // panic("Not implemented");
+
+  if (addr == 0) {
+    addr = (uintptr_t)heap.start;
+  }
+
+  void* ret = (void*) addr;
+  addr += size;
+
+  return ret;
+
+
 #endif
   return NULL;
 }
