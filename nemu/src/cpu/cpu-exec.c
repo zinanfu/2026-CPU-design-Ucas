97de1abebe4c636ce65545e4c1d7040134d2d4fb
tracer-ysyx <tracer@ysyx.org>
2026-05-23 11:10:18 +0800

    > compile NEMU 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  11:10:18 up  2:03,  1 user,  load average: 0.16, 0.07, 0.02

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -43,7 +43,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
 #ifdef CONFIG_WATCHPOINT
-  if (check_watchpoint()) {
+  if (check_watchpoint() && nemu_state.state == NEMU_RUNNING) {
     nemu_state.state = NEMU_STOP;
   }
 #endif
