adf0429dcb4b36f037619e78d58a91f7db3e67de
tracer-ysyx <tracer@ysyx.org>
2026-05-24 00:35:45 +0800

    > generate verilog 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  00:35:45 up  1:13,  0 users,  load average: 0.23, 0.16, 0.06

--- npc/src/register.scala
+++ npc/src/register.scala
@@ -6,21 +6,21 @@ import chisel3.util._
 class register(width:Int) extends Module {
     val io = IO(new Bundle{
         // read port 1
-        val raddr1 = Input(UInt(5.w))
-        val rdata1 = Output(UInt(32.w))
+        val raddr1 = Input(UInt(5.W))
+        val rdata1 = Output(UInt(32.W))
 
         // read port 2
-        val raddr2 = Input(UInt(5.w))
-        val rdata2 = Output(UInt(32.w))
+        val raddr2 = Input(UInt(5.W))
+        val rdata2 = Output(UInt(32.W))
 
         // write port
         val wen = Input(Bool())
-        val waddr = Input(UInt(5.w))
-        val wdata = Input(UInt(32.w))
+        val waddr = Input(UInt(5.W))
+        val wdata = Input(UInt(32.W))
 
     })
 
-    val regs = RegInit(VecInit(seq.fill(32)(0.U(32.w))))
+    val regs = RegInit(VecInit(seq.fill(32)(0.U(32.W))))
 
     // read
     io.rdata1 := regs(io.raddr1)
