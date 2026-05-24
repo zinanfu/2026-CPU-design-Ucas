4cc17a2086eb004d1b77bdac54c143fb427caf8d
tracer-ysyx <tracer@ysyx.org>
2026-05-24 15:47:48 +0800

    > sim RTL 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  15:47:48 up  1:49,  0 users,  load average: 0.07, 0.05, 0.01

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -36,8 +36,12 @@ int main(int argc, char** argv) {
     while (!Verilated::gotFinish()) {
 
         uint32_t pc = top->io_pc;
+        if (pc < 0x80000000 || pc > 0x80100000) {
+            printf("BAD PC = 0x%08x\n", pc);
+            break;
+        }
         uint32_t inst = paddr_read(pc,4);
-        printf("pc = 0x%08x inst = 0x%08x\n", pc, inst);
+        // printf("pc = 0x%08x inst = 0x%08x\n", pc, inst);
 
 
         // instruction fetch
