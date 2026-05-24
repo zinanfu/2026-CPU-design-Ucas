abe87ef548d488fdb7879e4114b39e4066503921
tracer-ysyx <tracer@ysyx.org>
2026-05-25 02:12:50 +0800

    > generate verilog 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  02:12:50 up  6:15,  1 user,  load average: 0.00, 0.02, 0.04

--- npc/src/mincpu.scala
+++ npc/src/mincpu.scala
@@ -307,8 +307,8 @@ class CpuTop extends Module {
       io.mem_wen := true.B
       io.mem_addr := store_addr & "hfffffffc".U(32.W)
 
-      printf(p"store_addr = 0x${Hexadecimal(store_addr)}\n")
-      printf(p"io.mem_addr = 0x${Hexadecimal(io.mem_addr)}\n")
+      // printf(p"store_addr = 0x${Hexadecimal(store_addr)}\n")
+      // printf(p"io.mem_addr = 0x${Hexadecimal(io.mem_addr)}\n")
 
       when(funct3 === "b000".U) { // sb
         io.mem_wmask := (1.U(4.W) << store_addr(1,0))
