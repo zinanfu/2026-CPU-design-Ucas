5e6c4e2a8e4222d1547e02b2d4cfd76dbd406e9a
tracer-ysyx <tracer@ysyx.org>
2026-05-25 00:51:04 +0800

    > sim RTL 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  00:51:04 up  4:53,  1 user,  load average: 0.45, 0.36, 0.23

---
+++ npc/obj_dir/VCpuTop__ConstPool__0__Slow.cpp
@@ -0,0 +1,15 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Constant pool
+
+#include "verilated.h"
+
+extern const VlUnpacked<CData/*3:0*/, 64> VCpuTop__ConstPool__TABLE_h01f0d32e_0 = {{
+    1U, 0U, 0x0fU, 0U, 3U, 0U, 0x0fU, 0U,
+    2U, 0U, 0x0fU, 0U, 3U, 0U, 0x0fU, 0U,
+    4U, 0U, 0x0fU, 0U, 0x0cU, 0U, 0x0fU, 0U,
+    8U, 0U, 0x0fU, 0U, 0x0cU, 0U, 0x0fU, 0U,
+    0U, 0U, 0x0fU, 0U, 3U, 0U, 0x0fU, 0U,
+    0U, 0U, 0x0fU, 0U, 3U, 0U, 0x0fU, 0U,
+    0U, 0U, 0x0fU, 0U, 0x0cU, 0U, 0x0fU, 0U,
+    0U, 0U, 0x0fU, 0U, 0x0cU, 0U, 0x0fU, 0U
+}};
