78a1162b6c1dab3435df3e927c02435f869db137
tracer-ysyx <tracer@ysyx.org>
2026-05-25 02:11:27 +0800

    > sim RTL 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  02:11:27 up  6:14,  1 user,  load average: 0.02, 0.02, 0.04

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <klib-macros.h>
+#include "../riscv.h"
 
 extern char _heap_start;
 int main(const char *args);
@@ -12,6 +13,8 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
 void putch(char ch) {
+  /* write to serial MMIO so simulator's paddr_write can catch it */
+  outb(0x10000000, (uint8_t)ch);
 }
 
 void halt(int code) {
