9d50b6aa96284e84c8558a082226f0a3342b210c
tracer-ysyx <tracer@ysyx.org>
2026-05-24 22:30:31 +0800

    > sim RTL 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  22:30:31 up  2:33,  0 users,  load average: 0.00, 0.00, 0.00

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char** argv) {
     // load image
     if (argc > 1) {
         load_image(argv[1], 0x80000000);
-        printf("first inst = 0x%08x\n", *(uint32_t *)pmem);
+        
     }
 
     // reset
