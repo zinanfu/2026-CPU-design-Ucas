7af4b830f63e89637b6e2d4df018f6fbc0d955cc
tracer-ysyx <tracer@ysyx.org>
2026-05-25 01:44:10 +0800

    > generate verilog 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  01:44:10 up  5:46,  1 user,  load average: 0.75, 0.35, 0.18

--- npc/src/mincpu.scala
+++ npc/src/mincpu.scala
@@ -137,10 +137,10 @@ class CpuTop extends Module {
       when(funct3 === "b101".U) {
         illegal := false.B
         wb_en := true.B
-        when(funct7 === "b0000000") { // srli
+        when(funct7 === "b0000000".U) { // srli
           wb_data := rs1_data >> immI
         }
-        when(funct7 === "b0100000") { // srai
+        when(funct7 === "b0100000".U) { // srai
           wb_data := (rs1_data.asSInt >> immI).asUInt
         }
         
