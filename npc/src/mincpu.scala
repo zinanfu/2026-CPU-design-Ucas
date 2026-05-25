ec38807ec715625547138b50d596b75df1efa745
tracer-ysyx <tracer@ysyx.org>
2026-05-25 17:05:03 +0800

    > generate verilog 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:05:03 up  2:03,  1 user,  load average: 0.01, 0.15, 0.10

--- npc/src/mincpu.scala
+++ npc/src/mincpu.scala
@@ -322,104 +322,100 @@ class CpuTop extends Module {
 
     // R_type
     is("b0110011".U) {
+      when(funct7 === "b0000000" || funct7 === "b0100000") {
+        when(funct3 === "b000".U) {
+          illegal := false.B
 
-      when(funct3 === "b000".U) {
-        illegal := false.B
+          wb_en := true.B
 
-        wb_en := true.B
+          when(funct7 === "b0000000".U) { //add
+            wb_data := rs1_data + rs2_data
+          }
 
-        when(funct7 === "b0000000".U) { //add
-          wb_data := rs1_data + rs2_data
+          when(funct7 === "b0100000".U) { //sub
+            wb_data := rs1_data - rs2_data
+          }
         }
+        when(funct3 === "b001".U) { //sll
+          illegal := false.B
 
-        when(funct7 === "b0100000".U) { //sub
-          wb_data := rs1_data - rs2_data
+          wb_en := true.B
+          wb_data := rs1_data << rs2_data(4, 0)
+        }
+        when(funct3 === "b101".U) { 
+          illegal := false.B
+          wb_en := true.B
+
+          when(funct7 === "b0000000".U) { //srl
+            wb_data := rs1_data >> rs2_data(4, 0)
+          }
+          when(funct7 === "b0100000".U) { //sra
+            wb_data := (rs1_data.asSInt >> rs2_data(4, 0)).asUInt
+          }
+        }
+        when(funct3 === "b011".U) { //sltu
+          illegal := false.B
+          wb_en := true.B
+
+          wb_data := (rs1_data < rs2_data).asUInt 
+        }
+        when(funct3 === "b010".U) { //slt
+          illegal := false.B
+          wb_en := true.B
+
+          wb_data := (rs1_data.asSInt < rs2_data.asSInt).asUInt 
+        }
+        when(funct3 === "b100".U) { //xor
+          illegal := false.B
+          wb_en := true.B
+
+          wb_data := rs1_data ^ rs2_data 
+        }
+        when(funct3 === "b110".U) { //or
+          illegal := false.B
+          wb_en := true.B
+
+          wb_data := rs1_data | rs2_data 
+        }
+        when(funct3 === "b111".U) { //and
+          illegal := false.B
+          wb_en := true.B
+
+          wb_data := rs1_data & rs2_data 
         }
       }
-      when(funct3 === "b001".U) { //sll
-        illegal := false.B
-
-        wb_en := true.B
-        wb_data := rs1_data << rs2_data(4, 0)
-      }
-      when(funct3 === "b101".U) { 
+      
+      // mul and div(rem)
+      when(funct7 === "b0000001".U) {
         illegal := false.B
         wb_en := true.B
 
-        when(funct7 === "b0000000".U) { //srl
-          wb_data := rs1_data >> rs2_data(4, 0)
+        when(funct3 === "b000".U) { //mul
+          wb_data := (rs1_data * rs2_data)(31, 0) 
         }
-        when(funct7 === "b0100000".U) { //sra
-          wb_data := (rs1_data.asSInt >> rs2_data(4, 0)).asUInt
+        when(funct3 === "b001".U) { //mulh
+          wb_data := (rs1_data.asSInt * rs2_data.asSInt)(63, 32).asUInt
         }
-      }
-      when(funct3 === "b011".U) { //sltu
-        illegal := false.B
-        wb_en := true.B
-
-        wb_data := (rs1_data < rs2_data).asUInt 
-      }
-      when(funct3 === "b010".U) { //slt
-        illegal := false.B
-        wb_en := true.B
-
-        wb_data := (rs1_data.asSInt < rs2_data.asSInt).asUInt 
-      }
-      when(funct3 === "b100".U) { //xor
-        illegal := false.B
-        wb_en := true.B
-
-        wb_data := rs1_data ^ rs2_data 
-      }
-      when(funct3 === "b110".U) { //or
-        illegal := false.B
-        wb_en := true.B
-
-        wb_data := rs1_data | rs2_data 
-      }
-      when(funct3 === "b111".U) { //and
-        illegal := false.B
-        wb_en := true.B
-
-        wb_data := rs1_data & rs2_data 
+        when(funct3 === "b010".U) { //mulhsu
+          wb_data := (rs1_data.asSInt * rs2_data)(63, 32).asUInt
+        }
+        when(funct3 === "b011".U) { //mulhu
+          wb_data := (rs1_data * rs2_data)(63, 32)
+        }
+        when(funct3 === "b100".U) { //div
+          wb_data := (rs1_data.asSInt / rs2_data.asSInt).asUInt
+        }
+        when(funct3 === "b101".U) { //divu
+          wb_data := rs1_data / rs2_data
+        }
+        when(funct3 === "b110".U) { //rem
+          wb_data := (rs1_data.asSInt % rs2_data.asSInt).asUInt
+        }
+        when(funct3 === "b111".U) { //remu
+          wb_data := rs1_data % rs2_data
+        }
       }
     }
-
-    // mul and div(rem)
-    is("b0110011".U) {
-      // illegal := false.B
-      // wb_en := true.B
-
-      // when(funct3 === "b000".U) { //mul
-      //   wb_data := (rs1_data * rs2_data)(31, 0) 
-      // }
-      // when(funct3 === "b001".U) { //mulh
-      //   wb_data := (rs1_data.asSInt * rs2_data.asSInt)(63, 32).asUInt
-      // }
-      // when(funct3 === "b010".U) { //mulhsu
-      //   wb_data := (rs1_data.asSInt * rs2_data)(63, 32).asUInt
-      // }
-      // when(funct3 === "b011".U) { //mulhu
-      //   wb_data := (rs1_data * rs2_data)(63, 32)
-      // }
-      // when(funct3 === "b100".U) { //div
-      //   wb_data := (rs1_data.asSInt / rs2_data.asSInt).asUInt
-      // }
-      // when(funct3 === "b101".U) { //divu
-      //   wb_data := rs1_data / rs2_data
-      // }
-      // when(funct3 === "b110".U) { //rem
-      //   wb_data := (rs1_data.asSInt % rs2_data.asSInt).asUInt
-      // }
-      // when(funct3 === "b111".U) { //remu
-      //   wb_data := rs1_data % rs2_data
-      // }
-
-
-    }
-
-
-
   }
 
   /*===========================wb=================================*/
