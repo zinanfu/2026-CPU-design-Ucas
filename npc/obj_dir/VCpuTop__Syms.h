6713e584d800f1c580aeebfa7cecb6e6c2f30b56
tracer-ysyx <tracer@ysyx.org>
2026-05-27 00:30:23 +0800

    > sim RTL 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  00:30:23 up  4:49,  1 user,  load average: 0.37, 0.37, 0.25

---
+++ npc/obj_dir/VCpuTop__Syms.h
@@ -0,0 +1,42 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Symbol table internal header
+//
+// Internal details; most calling programs do not need this header,
+// unless using verilator public meta comments.
+
+#ifndef VERILATED_VCPUTOP__SYMS_H_
+#define VERILATED_VCPUTOP__SYMS_H_  // guard
+
+#include "verilated.h"
+
+// INCLUDE MODEL CLASS
+
+#include "VCpuTop.h"
+
+// INCLUDE MODULE CLASSES
+#include "VCpuTop___024root.h"
+
+// DPI TYPES for DPI Export callbacks (Internal use)
+
+// SYMS CLASS (contains all model state)
+class alignas(VL_CACHE_LINE_BYTES) VCpuTop__Syms final : public VerilatedSyms {
+  public:
+    // INTERNAL STATE
+    VCpuTop* const __Vm_modelp;
+    bool __Vm_activity = false;  ///< Used by trace routines to determine change occurred
+    uint32_t __Vm_baseCode = 0;  ///< Used by trace routines when tracing multiple models
+    VlDeleter __Vm_deleter;
+    bool __Vm_didInit = false;
+
+    // MODULE INSTANCE STATE
+    VCpuTop___024root              TOP;
+
+    // CONSTRUCTORS
+    VCpuTop__Syms(VerilatedContext* contextp, const char* namep, VCpuTop* modelp);
+    ~VCpuTop__Syms();
+
+    // METHODS
+    const char* name() const { return TOP.vlNamep; }
+};
+
+#endif  // guard
