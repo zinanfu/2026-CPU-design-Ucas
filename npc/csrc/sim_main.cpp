9ca7cef7038dbef6e33fae7b08f4bef81f89dd82
tracer-ysyx <tracer@ysyx.org>
2026-06-05 22:28:11 +0800

    > sim RTL 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  22:28:11 up  3:44,  1 user,  load average: 0.21, 0.13, 0.09

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -34,7 +34,7 @@ int main(int argc, char** argv) {
     }
 
 #ifdef CONFIG_DIFFTEST
-    extern const char* diff_so_file = (argc > 3) ? argv[3] : nullptr;
+    const char* diff_so_file = (argc > 3) ? argv[3] : nullptr;
     size_t img_size = 128 * 1024 * 1024;
 
     if (diff_so_file) {
