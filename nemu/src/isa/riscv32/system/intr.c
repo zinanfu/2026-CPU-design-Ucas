b7bb9e25f43cd2f970c1b978f1685943294d9284
tracer-ysyx <tracer@ysyx.org>
2026-06-06 21:53:38 +0800

    > compile NEMU 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:53:38 up  7:38,  0 users,  load average: 0.08, 0.05, 0.01

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -14,7 +14,6 @@
 ***************************************************************************************/
 
 #include <isa.h>
-#include <stdio.h>
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
@@ -24,7 +23,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   cpu.mepc = epc;
   cpu.mcause = NO;
 
-  printf("intr: NO=0x%x, epc=0x%x, jump to mtvec=0x%x", NO, epc, cpu.mtvec);
+  Log("intr: NO=0x%x, epc=0x%x, jump to mtvec=0x%x", NO, epc, cpu.mtvec);
 
   return cpu.mtvec;
 }
