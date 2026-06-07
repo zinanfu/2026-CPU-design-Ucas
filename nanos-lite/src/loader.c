ca7f6279222c6f636c069f312216bd6b9373deb3
tracer-ysyx <tracer@ysyx.org>
2026-06-07 18:32:06 +0800

    > compile NEMU 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  18:32:06 up  1:50,  0 users,  load average: 0.04, 0.01, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,33 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+// ramdisk
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  // TODO();
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+
+  assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
+
+  for (int i = 0; i < ehdr.e_phnum; i++) {
+    Elf_Phdr phdr;
+    ramdisk_read(&phdr, ehdr.e_phoff + i * ehdr.e_phentsize, sizeof(Elf_Phdr));
+
+    if (phdr.p_type == PT_LOAD) {
+
+      ramdisk_read((void *)phdr.p_vaddr, phdr.p_offset, phdr.p_filesz);
+
+      if (phdr.p_memsz > phdr.p_filesz) {
+        memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
+      }
+    }
+  }
+  
+
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
