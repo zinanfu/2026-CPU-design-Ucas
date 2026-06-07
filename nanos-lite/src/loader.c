d616358371993342d3ac874d89b8ee869c6f1809
tracer-ysyx <tracer@ysyx.org>
2026-06-07 22:41:29 +0800

    > compile NEMU 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  22:41:29 up  5:59,  0 users,  load average: 0.06, 0.01, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,6 +9,21 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+
+#if defined(__ISA_AM_NATIVE__)
+# define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_X86__)
+# define EXPECT_TYPE EM_386
+#elif defined(__ISA_RISCV32__)
+# define EXPECT_TYPE EM_RISCV
+#elif defined(__ISA_MIPS32__)
+# define EXPECT_TYPE EM_MIPS
+#elif defined(__ISA_LOONGARCH32R__)
+# define EXPECT_TYPE EM_LOONGARCH
+#else
+# error Unsupported ISA
+#endif
+
 // ramdisk
 extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
@@ -19,6 +34,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
 
   assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
+  assert(ehdr.e_type == EXPECT_TYPE);
 
   for (int i = 0; i < ehdr.e_phnum; i++) {
     Elf_Phdr phdr;
