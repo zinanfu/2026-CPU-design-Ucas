1fc0eff7a18dcdbee1e684291ea862ec6920d31a
tracer-ysyx <tracer@ysyx.org>
2026-06-08 01:57:47 +0800

    > compile NEMU 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-111-generic #111~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 17:13:45 UTC  x86_64 x86_64 x86_64 GNU/Linux  01:57:47 up  9:16,  1 user,  load average: 0.06, 0.08, 0.05

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,6 +7,11 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
 
+#ifdef CONFIG_STRACE
+  Log("[strace] syscall ID:%d, arg1:%d, arg2:%d, arg3:%d\\n", a[0], a[1], a[2], a[3]);
+#endif
+
+
   switch (a[0]) {
     case 0: halt(a[0]); break;
     case 1: yield(); c->GPR2 = 0; break;  // sys_yield
