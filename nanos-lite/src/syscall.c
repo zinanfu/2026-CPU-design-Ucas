8b42bc5abe7a759dfb84dd47dd0a17aa50a4b970
tracer-ysyx <tracer@ysyx.org>
2026-06-11 09:45:58 +0800

    > compile NEMU 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  09:45:58 up 26 min,  0 users,  load average: 0.05, 0.07, 0.04

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,30 @@
 #include <common.h>
 #include "syscall.h"
+
+static void do_write(Context *c) {
+  uintptr_t a[4];
+  a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
+
+  int fd = a[1];
+  char *buf = (char *)a[2];
+  size_t len = a[3];
+
+  if (fd == 1 || fd == 2) {
+    for (size_t i = 0; i < len; i++) {
+      putch(buf[i]);
+    }
+    c->GPR2 = len;
+  }
+  else {
+    c->GPR2 = -1;
+  }
+
+
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -15,8 +40,11 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case 0: halt(a[0]); break;
     case 1: yield(); c->GPR2 = 0; break;  // sys_yield
-    
+
+    case 4: do_write(c); break;
 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
+
+
