08af83a4f95abe8954d2655e2ddc9cb255be804c
tracer-ysyx <tracer@ysyx.org>
2026-06-14 17:43:58 +0800

    > compile NEMU 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:43:58 up  6:00,  1 user,  load average: 0.07, 0.09, 0.03

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -58,10 +58,10 @@ int fs_open(const char *pathname, int flags, int mode) {
   int range = sizeof(file_table) / sizeof(Finfo);
   // printf("range = %d\n", range);
   for (int i = 0; i < range; i++) {
-    // printf("there is for pathname = %s\n", pathname);
+    printf("there is for pathname = %s\n", pathname);
     if (strcmp(pathname, file_table[i].name) == 0) {
       fs_d[i].open_offset = 0;
-      // printf("fd == %d\n", i);
+      printf("fd == %d\n", i);
       return i;
     }
   }
