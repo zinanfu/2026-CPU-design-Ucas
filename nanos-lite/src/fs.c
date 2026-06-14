6b79f76dd2ba03fcee6afd23ee1e7ccda68a0b78
tracer-ysyx <tracer@ysyx.org>
2026-06-14 17:45:08 +0800

    > compile NEMU 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:45:08 up  6:01,  1 user,  load average: 0.18, 0.11, 0.04

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -83,12 +83,12 @@ size_t fs_read(int fd, void *buf, size_t len) {
   //   all_size = file_table[fd].size - inner_file_offset;
   //   out_of_bond = 1;
   // }
-  // printf("read begin\n");
-  // printf("fd == %d\n", fd);
+  printf("read begin\n");
+  printf("fd == %d\n", fd);
   size_t ret = file_table[fd].read(buf, inner_file_offset + offset, len);
   fs_d[fd].open_offset += ret;
-  // printf("read done\n");
-  // printf("ret = %d\n", ret);
+  printf("read done\n");
+  printf("ret = %d\n", ret);
   // return (out_of_bond ==  1) ? 0 : ret;
   return ret;
 }
