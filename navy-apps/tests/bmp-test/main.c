5f682bf8e1402c7ad3402f9326a5b484b4c2c2d7
tracer-ysyx <tracer@ysyx.org>
2026-06-14 17:43:01 +0800

    > compile NEMU 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:43:01 up  5:59,  1 user,  load average: 0.19, 0.11, 0.04

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -7,7 +7,9 @@
 int main() {
   NDL_Init(0);
   int w, h;
+  printf("hello bmp-test\n");
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
+  printf("load done\n");
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
 
