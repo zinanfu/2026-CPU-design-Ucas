6c3d5b4cbba06ec7f6c5c78de87cea39531a46ab
tracer-ysyx <tracer@ysyx.org>
2026-06-14 17:42:03 +0800

    > compile NEMU 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:42:03 up  5:58,  1 user,  load average: 0.11, 0.09, 0.03

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -35,6 +35,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
+  printf("vag read len = %d\n", len);
   AM_GPU_CONFIG_T cfg = io_read(AM_GPU_CONFIG);
 
   return sprintf(buf, "WIDTH : %d\nHEIGHT : %d\n", cfg.width, cfg.height);
