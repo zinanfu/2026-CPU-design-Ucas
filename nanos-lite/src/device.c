f2cb39e91be1894b110205b1e61f25931b742808
tracer-ysyx <tracer@ysyx.org>
2026-06-14 10:48:52 +0800

    > compile NEMU 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  10:48:52 up 28 min,  0 users,  load average: 0.08, 0.02, 0.02

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,6 +15,7 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
+  printf("serial write: len = %d\n", len);
   const char *p = (const char *)buf;
   for (size_t i = 0; i < len; i++) {
     putch(p[i]);
