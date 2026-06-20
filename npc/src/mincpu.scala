ead0f86e5bb028fcdf792747c83cf9926ad49d01
tracer-ysyx <tracer@ysyx.org>
2026-06-21 02:14:28 +0800

    > generate verilog 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  02:14:28 up  2:52,  0 users,  load average: 0.17, 0.13, 0.09

--- npc/src/mincpu.scala
+++ npc/src/mincpu.scala
@@ -509,7 +509,7 @@ class CpuTop(enableItrace: Boolean = true) extends Module {
   }
   when(pc === "h80019100".U) {
     printf("pc=0x%8x, next_pc=0x%8x, inst=0x%8x\n", pc, next_pc, inst)
-    printf("sp=0x%8x\n", regs[2.U])
+    printf("sp=0x%8x\n", regs(2.U))
   }
 
   // printf("pc=0x%8x, next_pc=0x%8x, inst=0x%8x\n", pc, next_pc, inst)
