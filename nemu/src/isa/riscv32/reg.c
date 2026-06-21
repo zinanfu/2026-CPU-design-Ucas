6f8b9452b9aeeac3ab6cac0897affffc13dfd7f7
tracer-ysyx <tracer@ysyx.org>
2026-06-21 15:25:43 +0800

    > compile NEMU 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  15:25:43 up  5:19,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -31,10 +31,14 @@ void isa_reg_display() {
     else {
       printf("$%s:\t0x%08x\n", regs[i], cpu.gpr[i]);
     }
+
     
   }
 
   printf("pc:\t0x%08x\n", cpu.pc);
+  printf("mepc:\t0x%08x\n", cpu.mepc);
+  printf("mstatus:\t0x%08x\n", cpu.mstatus);
+  printf("mcause:\t0x%08x\n", cpu.mcause);
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
