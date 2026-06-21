80f1c93eb6a250b6000df6677c2a350f8aa2cede
tracer-ysyx <tracer@ysyx.org>
2026-06-21 23:06:50 +0800

    > compile NEMU 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  23:06:50 up  4:47,  0 users,  load average: 0.08, 0.02, 0.01

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -116,7 +116,7 @@ static int decode_exec(Decode *s) {
   // ecall
   INSTPAT("0000000 00000 00000 000 00000 1110011", ecall   , N, s->dnpc = isa_raise_intr(0xb, s->pc));
   // mret
-  INSTPAT("0011000 00010 00000 000 00000 1110011", mret    , N, cpu.mstatus = (cpu.mstatus & ~0x8) | ((cpu.mstatus & 0x80) >> 4); s->dnpc = cpu.mepc);
+  INSTPAT("0011000 00010 00000 000 00000 1110011", mret    , N, cpu.mstatus = (cpu.mstatus & ~0x8) | ((cpu.mstatus & 0x80) >> 4); s->dnpc = cpu.mepc + 4);
 
   INSTPAT("0000000 00001 00000 000 00000 1110011", ebreak  , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   // ALU
