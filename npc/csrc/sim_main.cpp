50ae1ca5d06f90ae0b097760636686c2a527a9e8
tracer-ysyx <tracer@ysyx.org>
2026-06-30 21:09:17 +0800

    > sim RTL 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:09:17 up 10:07,  0 users,  load average: 0.00, 0.00, 0.00

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -190,10 +190,10 @@ void repl_loop(VCpuTop* top) {
                 n = 1;
             }
             for (int i = 0; i < n; i++) {
-                bool sign = step_once(top);
+                
                 printf("pc  =0x%8x\n", top->io_pc);
                 printf("inst=0x%08x\n", top->io_inst);
-
+                bool sign = step_once(top);
 
                 if (Verilated :: gotFinish()) {
                     break;
