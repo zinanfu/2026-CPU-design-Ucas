5dc9c756a6fec01dff5ed05da8ebed0531fd1d51
tracer-ysyx <tracer@ysyx.org>
2026-07-04 01:10:06 +0800

    > generate verilog 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  01:10:06 up  3:52,  0 users,  load average: 0.18, 0.19, 0.10

--- npc/src/singleMemory.scala
+++ npc/src/singleMemory.scala
@@ -20,9 +20,9 @@ class singleMemory(width:Int) extends Module {
     val regs = RegInit(VecInit(Seq.fill(32)(0.U(32.W))))
     val paddrRead = Module(new PaddrReadDPI)
 
-    paddrRead.io.addr = io.addr
-    paddrRead.io.len  = io.len
-    paddrRead.io.is_inst = io.is_inst
+    paddrRead.io.addr := io.addr
+    paddrRead.io.len  := io.len
+    paddrRead.io.is_inst := io.is_inst
     // read
     io.rdata := paddrRead.io.data
 
