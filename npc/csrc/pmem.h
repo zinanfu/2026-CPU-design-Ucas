4f4d0e16aca81a19db588c1fa07536ebd99f3c05
tracer-ysyx <tracer@ysyx.org>
2026-07-04 01:23:38 +0800

    > sim RTL 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  01:23:38 up  4:05,  0 users,  load average: 0.65, 0.61, 0.37

--- npc/csrc/pmem.h
+++ npc/csrc/pmem.h
@@ -11,7 +11,7 @@ void init_pmem(size_t size, uint32_t base);
 void free_pmem();
 bool load_image(const char *path, uint32_t load_addr);
 
-extern "C" paddr_read(uint32_t addr, int len, bool is_inst);
+extern "C" uint32_t paddr_read(uint32_t addr, int len, bool is_inst);
 void paddr_write(uint32_t addr, int len, uint32_t data, uint8_t wmask, bool is_inst);
 const uint8_t* get_pmem_ptr();
 
