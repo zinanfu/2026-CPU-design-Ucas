c4136305b4d1dbe64a52fd626306d89b735ad732
tracer-ysyx <tracer@ysyx.org>
2026-07-05 02:01:57 +0800

    > generate verilog 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  02:01:57 up  7:06,  1 user,  load average: 0.34, 0.09, 0.03

--- npc/src/Elaborate.scala
+++ npc/src/Elaborate.scala
@@ -38,7 +38,7 @@ object Elaborate extends App {
   val (enableItrace, chiselArgs) = parseItraceArgs(args)
 
   ChiselStage.emitSystemVerilogFile(
-    new ysyx_09929017(enableItrace),
+    new ysyx_26050162(enableItrace),
     chiselArgs,
     firtoolOptions
   )
