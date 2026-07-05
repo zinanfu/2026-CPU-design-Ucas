3903b2c132b8ca4c26a4647d3adc0fcb15f52eb9
tracer-ysyx <tracer@ysyx.org>
2026-07-05 22:55:43 +0800

    > sim RTL 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  22:55:43 up 11:40,  1 user,  load average: 0.08, 0.09, 0.07

--- npc/csrc/pmem.cpp
+++ npc/csrc/pmem.cpp
@@ -120,8 +120,8 @@ extern "C" void paddr_write(uint32_t addr, int len, uint32_t data, uint8_t wmask
     assert(len == 1 || len == 2 || len == 4);
     
     // Debug: print every physical write to help trace MMIO vs PMEM
-    // printf("paddr_write called addr = 0x%08x len = %d data = 0x%08x wmask = 0x%x\n",
-    //        addr, len, data, wmask);
+    printf("paddr_write called addr = 0x%08x len = %d data = 0x%08x wmask = 0x%x\n",
+           addr, len, data, wmask);
     if (!in_pmem(addr, len)) {
 
         if ((addr & ~0x3) == SERIAL_PORT) {
