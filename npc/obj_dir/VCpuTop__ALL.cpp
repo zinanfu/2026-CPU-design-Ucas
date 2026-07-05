2aaf28f9e4481ad5aaf1c80f87d462fac3086aee
tracer-ysyx <tracer@ysyx.org>
2026-07-05 21:48:53 +0800

    > sim RTL 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:48:53 up 10:33,  1 user,  load average: 0.27, 0.16, 0.17

--- npc/obj_dir/VCpuTop__ALL.cpp
+++ npc/obj_dir/VCpuTop__ALL.cpp
@@ -4,6 +4,7 @@
 #include "VCpuTop___024root__0.cpp"
 #include "VCpuTop__Dpi.cpp"
 #include "VCpuTop__Trace__0.cpp"
+#include "VCpuTop__ConstPool__0__Slow.cpp"
 #include "VCpuTop___024root__Slow.cpp"
 #include "VCpuTop___024root__0__Slow.cpp"
 #include "VCpuTop__Syms__Slow.cpp"
