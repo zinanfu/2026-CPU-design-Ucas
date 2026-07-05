a35b175eaee306d4248e438c910062a86f609111
tracer-ysyx <tracer@ysyx.org>
2026-07-05 22:47:53 +0800

    > generate verilog 2023K8009929017 傅子楠 Linux nan-virtual-machine 6.8.0-124-generic #124~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May 26 21:05:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  22:47:53 up 11:32,  1 user,  load average: 0.06, 0.03, 0.00

--- npc/src/AXIsram.scala
+++ npc/src/AXIsram.scala
@@ -15,6 +15,9 @@ class AXIsram(is_inst: Boolean = false) extends Module {
   val paddrWrite = Module(new PaddrWriteDPI)
 
   // read
+  val rvalidReg = RegInit(false.B)
+  val rdataReg  = RegInit(0.U(32.W))
+
   io.axi.ar.ready := true.B
 
   val arFire = io.axi.ar.valid && io.axi.ar.ready
@@ -26,8 +29,15 @@ class AXIsram(is_inst: Boolean = false) extends Module {
   val arFire_d = RegNext(arFire, false.B)
   val rdata_d  = RegNext(paddrRead.io.data, 0.U(32.W))
 
-  io.axi.r.valid := arFire_d
-  io.axi.r.data  := rdata_d
+  when (arFire) {
+    rvalidReg := true.B
+    rdataReg  := paddrRead.io.data
+  }.elsewhen (io.axi.r.valid && io.axi.r.ready) {
+    rvalidReg := false.B
+  }
+
+  io.axi.r.valid := rvalidReg
+  io.axi.r.data  := rdataReg
   io.axi.r.resp  := 0.U
 
   // write
@@ -36,7 +46,9 @@ class AXIsram(is_inst: Boolean = false) extends Module {
 
   val awFire    = io.axi.aw.valid && io.axi.aw.ready
   val wFire     = io.axi.w.valid  && io.axi.w.ready
+
   val writeFire = awFire && wFire
+  val bvalidreg = RegInit(false.B)
 
   paddrWrite.io.addr    := Mux(writeFire, io.axi.aw.addr, 0.U)
   paddrWrite.io.data    := io.axi.w.data
@@ -52,8 +64,16 @@ class AXIsram(is_inst: Boolean = false) extends Module {
   ))
   paddrWrite.io.is_inst := is_inst.B
 
-  val writeFire_d = RegNext(writeFire, false.B)
+  when (writeFire) {
+    bvalidreg := true.B
+  }.elsewhen (io.axi.b.valid && io.axi.b.ready) {
+    bvalidreg := false.B
+  }
 
-  io.axi.b.valid := writeFire_d
-  io.axi.b.resp  := 0.U
+  io.axi.b.valid := bvalidreg
+
+  // val writeFire_d = RegNext(writeFire, false.B)
+
+  // io.axi.b.valid := writeFire_d
+  // io.axi.b.resp  := 0.U
 }
